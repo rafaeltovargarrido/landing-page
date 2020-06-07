911d7519e3256e6027a64272b830552f2e694a78
Rafael Tovar <rafaeltovar1992@gmail.com>
2020-06-07 15:15:22 +0200

    nuevo valor

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'rafa';
+	public $baseURL = 'http://dev.redindev.com/';
 
 	/*
 	|--------------------------------------------------------------------------
